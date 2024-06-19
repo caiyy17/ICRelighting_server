2299df885fbfc1234fa59c88714c42710d568df5
cyy <caiyiyi1998@gmail.com>
2024-06-19 13:37:12 +0900

    server

---
+++ comfy_api.py
@@ -0,0 +1,76 @@
+#This is an example that uses the websockets api and the SaveImageWebsocket node to get images directly without
+#them being saved to disk
+
+import websocket #NOTE: websocket-client (https://github.com/websocket-client/websocket-client)
+import uuid
+import json
+import urllib.request
+import urllib.parse
+
+server_address = "127.0.0.1:8188"
+client_id = str(uuid.uuid4())
+
+def queue_prompt(prompt):
+    p = {"prompt": prompt, "client_id": client_id}
+    data = json.dumps(p).encode('utf-8')
+    req =  urllib.request.Request("http://{}/prompt".format(server_address), data=data)
+    return json.loads(urllib.request.urlopen(req).read())
+
+def get_image(filename, subfolder, folder_type):
+    data = {"filename": filename, "subfolder": subfolder, "type": folder_type}
+    url_values = urllib.parse.urlencode(data)
+    with urllib.request.urlopen("http://{}/view?{}".format(server_address, url_values)) as response:
+        return response.read()
+
+def get_history(prompt_id):
+    with urllib.request.urlopen("http://{}/history/{}".format(server_address, prompt_id)) as response:
+        return json.loads(response.read())
+
+def get_images(ws, prompt):
+    prompt_id = queue_prompt(prompt)['prompt_id']
+    output_images = {}
+    current_node = ""
+    while True:
+        out = ws.recv()
+        if isinstance(out, str):
+            message = json.loads(out)
+            if message['type'] == 'executing':
+                data = message['data']
+                if data['prompt_id'] == prompt_id:
+                    if data['node'] is None:
+                        break #Execution is done
+                    else:
+                        current_node = data['node']
+        else:
+            print(current_node)
+            # if current_node == 'save_image_websocket_node':
+            if True:
+                images_output = output_images.get(current_node, [])
+                images_output.append(out[8:])
+                output_images[current_node] = images_output
+
+    return output_images
+
+def comfy_generate(path):
+    with open("workflow_api.json", "r", encoding="utf-8") as f:
+        prompt_text = f.read()
+
+    prompt = json.loads(prompt_text)
+
+    #set the text prompt for our positive CLIPTextEncode
+    prompt["9"]["inputs"]["image"] = "../../ICRelighting_server/{}".format(path)
+
+    ws = websocket.WebSocket()
+    ws.connect("ws://{}/ws?clientId={}".format(server_address, client_id))
+    images = get_images(ws, prompt)
+
+    # save images to disk
+    for node_id in images:
+        index = 0
+        for image_data in images[node_id]:
+            from PIL import Image
+            import io
+            image = Image.open(io.BytesIO(image_data))
+            image.save(f"uploads/output_{node_id}_{index}.png")
+            index += 1
+
--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 import os
+import base64
+import io
+from PIL import Image
+from flask import jsonify
+
+import comfy_api as comfy_api
 
 app = Flask(__name__)
 
@@ -26,12 +32,59 @@ def upload_file():
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         print('File successfully uploaded')
-        # change it into png
-        os.system('ffmpeg -i ' + os.path.join(app.config['UPLOAD_FOLDER'], filename) + ' ' + os.path.join(app.config['UPLOAD_FOLDER'], 'image' + '.png'))
+        # change it into png, -y is to overwrite the existing file
+        os.system(f"ffmpeg -i {UPLOAD_FOLDER + filename} {UPLOAD_FOLDER}image.png -y")
         print('File successfully converted')
         return 'File successfully uploaded'
     else:
         return 'Allowed file types are png, jpg, jpeg, gif'
+
+@app.route('/process', methods=['POST'])
+def process_image():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
+    
+    image = request.files['image']
+    if image.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+    
+    filepath = os.path.join(UPLOAD_FOLDER, image.filename)
+    image.save(filepath)
+
+    comfy_api.comfy_generate(filepath)
+
+    # 在这里处理图像并生成四张Base64图片（此处为示例，实际处理可能不同）
+    base64_images = []
+    image_names = ["152_0.png", "152_1.png", "152_3.png", "152_2.png", "153_0.png"]
+    for i in range(5):
+        buffer = io.BytesIO()
+        Image.open(UPLOAD_FOLDER + f"output_{image_names[i]}").save(buffer, format="PNG")
+        base64_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        base64_images.append(base64_image)
+    
+    response_data = {
+        "message": "Image received successfully",
+        "status": "success",
+        "images": base64_images
+    }
+    
+    return jsonify(response_data)
+
+@app.route('/sync', methods=['POST'])
+def sync_image():
+    image_name = UPLOAD_FOLDER + "image.png"
+    buffer = io.BytesIO()
+    Image.open(image_name).save(buffer, format="PNG")
+    base64_images = []
+    base64_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    base64_images.append(base64_image)
+
+    response_data = {
+        "message": "Image send successfully",
+        "status": "success",
+        "images": base64_images
+    }
+    return jsonify(response_data)
 
 if __name__ == '__main__':
     # make sure the folder exists
