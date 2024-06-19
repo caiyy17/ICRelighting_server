b25919576b5c72c757d90fe8fe33d400bc0df4f5
CYY <caiyiyi1998@gmail.com>
2024-06-19 12:03:41 +0900

    first

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def upload_form():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        print('File successfully uploaded')
+        # change it into png
+        os.system('ffmpeg -i ' + os.path.join(app.config['UPLOAD_FOLDER'], filename) + ' ' + os.path.join(app.config['UPLOAD_FOLDER'], 'image' + '.png'))
+        print('File successfully converted')
+        return 'File successfully uploaded'
+    else:
+        return 'Allowed file types are png, jpg, jpeg, gif'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
